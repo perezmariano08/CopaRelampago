2cb623105cfe9a97e91d0f02069cfcea2790cdef
perezmarianopm <perezmariano.pm@gmail.com>
2024-02-08 20:28:15 -0300

    changes input date

--- src/pages/CreateAccount/CreateAccount.jsx
+++ src/pages/CreateAccount/CreateAccount.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CreateAccountContainerStyled, CreateAccountData, CreateAccountInputs, CreateAccountWrapper } from './CreateAccountStyles'
 import Input from '../../components/UI/Input/Input'
-import { HiKey, HiUser } from 'react-icons/hi2'
+import { HiCalendar, HiKey, HiOutlineCreditCard, HiUser, HiPhone, HiEnvelope, HiOutlineShieldCheck } from 'react-icons/hi2'
 import Button from '../../components/UI/Button/Button'
 import { NavLink } from 'react-router-dom'
 import InputPassword from '../../components/UI/Input/InputPassword'
@@ -14,15 +14,16 @@ const CreateAccount = () => {
                 <CreateAccountData>
                     <h2>Creá tu cuenta</h2>
                     <CreateAccountInputs>
-                        <Input placeholder={'DNI'}><HiUser/></Input>
+                        <Input placeholder={'DNI'}><HiOutlineCreditCard/></Input>
                         <Input placeholder={'Nombre'}><HiUser/></Input>
                         <Input placeholder={'Apellido'}><HiUser/></Input>
-                        <Input type="date" placeholder={'Fecha de nacimiento'}><HiUser/></Input>
-                        <Input type="email" placeholder={'Email'}><HiUser/></Input>
+                        <input type="date" />
+                        <Input type="date" placeholder={'Fecha de nacimiento'}><HiCalendar/></Input>
+                        <Input placeholder={'Telefono'}><HiPhone/></Input>
+                        <Input type="email" placeholder={'Email'}><HiEnvelope/></Input>
                         <Select>
-                            <HiKey/>
+                            <HiOutlineShieldCheck/>
                         </Select>
-                        <Input placeholder={'DNI'}><HiUser/></Input>
                         <InputPassword placeholder='Contraseña'>
                             <HiKey/>
                         </InputPassword>
