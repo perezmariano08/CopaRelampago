f9b91a32c8ce72ea2adb0251197007d760dc2120
Joaquin Pozzo <pozzojoa@gmail.com>
2024-02-21 11:43:33 -0300

    notis-3

--- src/components/Navbar/Notifications/NotificationsStyles.js
+++ src/components/Navbar/Notifications/NotificationsStyles.js
@@ -49,7 +49,7 @@ export const NotificationsContainer = styled.div`
     gap: 15px;
     padding: 15px;
     background-color: var(--gray-400);
-    max-height: 70%;
+    max-height: 600px;
     overflow-y: auto;
 `;
 
